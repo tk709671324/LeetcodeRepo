c1797128286e9f02c7da1ab0269879edf925a14f
tk709671324 <chenyixiu26@gmail.com>
2018-11-06 16:15:22 -0500

    344_Reverse_String

--- 344_Reverse_String/Java/Solution_1.java
+++ 344_Reverse_String/Java/Solution_1.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Solution_1 {
+	/**
+	 *
+	 * Date : 2018/11/6
+	 * beats : 45.77%
+	 *
+	 * time complexity:O(n)
+	 */
+	public String reverseString(String s) {
+		StringBuilder res = new StringBuilder();
+		for(int i = s.length() - 1; i >= 0; i--){
+			res.append(s.charAt(i));
+		}
+
+		return res.toString();
+	}
+
+	public static void main(String[] args) {
+
+		Solution_1 solution_1 = new Solution_1();
+		String s = "";
+		String a = solution_1.reverseString(s);
+		System.out.println(a);
+	}
 }
--- 344_Reverse_String/Java/Solution_2.java
+++ 344_Reverse_String/Java/Solution_2.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
-
 public class Solution_2 {
+
+	/**
+	 *
+	 * Date : 2018/11/6
+	 * beats : 100%
+	 *
+	 * time complexity:O(n)
+	 *
+	 */
+	public String reverseString(String s){
+
+		if(s == null || s.length() == 0){
+			return "";
+		}
+
+		int l = 0;
+		int r = s.length() - 1;
+		char[] res = s.toCharArray();
+		while(l < r){
+			swap(res, l, r);
+			l++;
+			r--;
+		}
+
+		return new String(res);
+	}
+
+	public void swap(char[] res, int l, int r){
+		char temp = res[l];
+		res[l] = res[r];
+		res[r] = temp;
+	}
+
+	public static void main(String[] args) {
+
+		Solution_2 solution_2 = new Solution_2();
+		String s = "\"A man\n" +
+				"a plan\n" +
+				"a canal: Panama\"";
+		String a = solution_2.reverseString(s);
+		System.out.println(a);
+	}
+
 }
