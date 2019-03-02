8fdd6c8e24ab26ae0c71a7daa30d258735292624
tk709671324 <chenyixiu26@gmail.com>
2019-03-02 11:44:38 -0500

    153. Find Minimum in Rotated Sorted Array

---
+++ 153_Find_Minimum_in_Rotated_Sorted_Array/Solution_1.java
@@ -0,0 +1,30 @@
+public class Solution_1 {
+    public int findMin(int[] nums) {
+
+//        Runtime: 0 ms, faster than 100.00% of Java online submissions for Find Minimum in Rotated Sorted Array.
+//        Memory Usage: 36.6 MB, less than 71.99% of Java online submissions for Find Minimum in Rotated Sorted Array.
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        //数组分为两部分， 左边上升部分所有值都比右边都大
+
+        int start = 0;
+        int end = nums.length - 1;
+        if (nums[end] > nums[start]) {
+            //如果没有翻转则直接返回最小值
+            return nums[start];
+        }
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[start]) {
+                //在上升部分
+                start = mid;
+            } else {
+                //在下降部分
+                start = end;
+            }
+        }
+
+        return nums[start];
+    }
+}
