03efaba9a5eee0cd770e205ba1f536cf3a421c63
tk709671324 <chenyixiu26@gmail.com>
2019-03-11 17:11:49 -0400

    18_4Sum

--- 876_Middle_of_the_Linked_List/Solution_1.java
+++ 876_Middle_of_the_Linked_List/Solution_1.java
@@ -2,6 +2,9 @@ public class Solution_1 {
 
 //    Runtime: 0 ms, faster than 100.00% of Java online submissions for Middle of the Linked List.
 //    Memory Usage: 36.7 MB, less than 60.86% of Java online submissions for Middle of the Linked List.
+
+
+    //如果求左边那个中值就使用dummyHead
     public ListNode middleNode(ListNode head) {
 
         ListNode middle = head;
